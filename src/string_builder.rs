b063ffbf31b9e967039156f5488b6f2f9397483e
patrick <patrick.thorbauer@student.tugraz.at>
2022-06-28 22:00:12 +0200

    edit build and info string

--- src/string_builder.rs
+++ src/string_builder.rs
@@ -1,16 +1,20 @@
 use regex::Captures;
 
 pub fn get_info_string(matches: Captures) -> (String, i32, i32) {
-    let mut info_string = String::from("Step ");
+    let mut info_string = String::from("");
     info_string.push_str(&matches[1]);
     info_string.push_str("/");
     info_string.push_str(&matches[2]);
     info_string.push_str(" ");
-    (info_string, (&matches[1]).parse().unwrap(), (&matches[2]).parse().unwrap())
+    (
+        info_string,
+        (&matches[1]).parse().unwrap(),
+        (&matches[2]).parse().unwrap(),
+    )
 }
 
 pub fn get_build_string(matches: Captures, build_count: &mut i32) -> String {
-    let mut build_string = String::from("Build ");
+    let mut build_string = String::from("");
     *build_count += 1;
     build_string.push_str(&*build_count.to_string());
     build_string.push_str("(");
